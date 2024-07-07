457e79e1d8b86d4496bf29ed3396cbcdbfc43c7b
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-07-07 01:09:05 -0700

    vite.config.js has fixed

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
